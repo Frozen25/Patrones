4d9d48fd70fe91925e94d70212da5c120595c15c
Alexis <alexis_gavriel@hotmail.com>
2019-10-10 14:25:08 -0600

    Se agrega el reset del número para evitar correr de nuevo

--- tarea4/src/keras/draw.py
+++ tarea4/src/keras/draw.py
@@ -3,36 +3,42 @@ from tkinter import *
 import PIL
 from PIL import Image, ImageDraw
 
+size = 200
 
 def save():    
-    filename = 'number.png'   # image_number increments by 1 at every save
-    image2 = image1.resize((28, 28),2) #segundo parámetro es 2 para bilineal o 3 para bicubico
-    image2.save(filename)
-    
+	filename = 'number.png'  
+	image2 = image1.resize((28, 28),2) #segundo parámetro es 2 para bilineal o 3 para bicubico
+	image2.save(filename)
+	
+def reset():
+	global size
+	cv.delete("all")
+	draw.rectangle((0, 0, size, size), fill=(255))
+
 def activate_paint(e):
-    global lastx, lasty
-    cv.bind('<B1-Motion>', paint)
-    lastx, lasty = e.x, e.y
+	global lastx, lasty
+	cv.bind('<B1-Motion>', paint)
+	lastx, lasty = e.x, e.y
 
 def paint(e):
-    global lastx, lasty
-    x, y = e.x, e.y
-    cv.create_oval((lastx, lasty, x, y), width=15)
-    #  --- PIL
-    draw.line((lastx, lasty, x, y), fill='black', width=15)
-    lastx, lasty = x, y
+	global lastx, lasty
+	x, y = e.x, e.y
+	cv.create_oval((lastx, lasty, x, y), width=15) 	
+	#  --- PIL
+	draw.line((lastx, lasty, x, y), fill='black', width=15)
+	lastx, lasty = x, y
 
 root = Tk()
 lastx, lasty = None, None
-cv = Canvas(root, width=200, height=200, bg='white')
+cv = Canvas(root, width=size, height=size, bg='white')
 # --- PIL
-image1 = PIL.Image.new('L', (200, 200), 'white')
+image1 = PIL.Image.new(mode='L', size=(size, size), color='white') # L = escala de grises
 draw = ImageDraw.Draw(image1)
-#image2 = image1.resize((28,28),resample=1)
 cv.bind('<1>', activate_paint)
 cv.pack(expand=YES, fill=BOTH)
 
 btn_save = Button(text="save", command=save)
 btn_save.pack()
-
+btn_reset = Button(text="reset", command=reset)
+btn_reset.pack()
 root.mainloop()
