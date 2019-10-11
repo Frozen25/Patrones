560ec71171ff370f6736c05aaa1f66740ffaafd2
AndresRQ27 <andrew.rq+github@gmail.com>
2019-10-10 20:19:25 -0600

    Mejora en formato

--- tarea4/src/svm/draw.py
+++ tarea4/src/svm/draw.py
@@ -2,38 +2,46 @@ import tkinter
 from tkinter import *
 import PIL
 from PIL import Image, ImageDraw
+import cv2
 
 size = 200
 
-def save():    
-	filename = 'resource/number.png'  
-	image2 = image1.resize((28, 28),2) #segundo parámetro es 2 para bilineal o 3 para bicubico
-	image2.save(filename)
-	
+
+def save():
+    filename = 'resource/number.png'
+    # segundo parámetro es 2 para bilineal o 3 para bicubico
+    image2 = image1.resize((28, 28), 2)
+    image2.save(filename)
+
+
 def reset():
-	global size
-	cv.delete("all")
-	draw.rectangle((0, 0, size, size), fill=(255))
+    global size
+    cv.delete("all")
+    draw.rectangle((0, 0, size, size), fill=(255))
+
 
 def activate_paint(e):
-	global lastx, lasty
-	cv.bind('<B1-Motion>', paint)
-	lastx, lasty = e.x, e.y
+    global lastx, lasty
+    cv.bind('<B1-Motion>', paint)
+    lastx, lasty = e.x, e.y
+
 
 def paint(e):
-	global lastx, lasty
-	thickness = 20
-	x, y = e.x, e.y
-	cv.create_oval((lastx, lasty, x, y), width=thickness) 	
-	#  --- PIL
-	draw.line((lastx, lasty, x, y), fill='black', width=thickness)
-	lastx, lasty = x, y
+    global lastx, lasty
+    thickness = 20
+    x, y = e.x, e.y
+    cv.create_oval((lastx, lasty, x, y), width=thickness)
+    #  --- PIL
+    draw.line((lastx, lasty, x, y), fill='black', width=thickness)
+    lastx, lasty = x, y
+
 
 root = Tk()
 lastx, lasty = None, None
 cv = Canvas(root, width=size, height=size, bg='white')
 # --- PIL
-image1 = PIL.Image.new(mode='L', size=(size, size), color='white') # L = escala de grises
+image1 = PIL.Image.new(mode='L', size=(size, size),
+                       color='white')  # L = escala de grises
 draw = ImageDraw.Draw(image1)
 cv.bind('<1>', activate_paint)
 cv.pack(expand=YES, fill=BOTH)
