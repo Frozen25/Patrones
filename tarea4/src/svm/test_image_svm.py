e5454125fa397b2fe764400d664a07ec419bb5db
AndresRQ27 <andrew.rq+github@gmail.com>
2019-10-11 15:42:37 -0600

    draw integrado en test

--- tarea4/src/svm/test_image_svm.py
+++ tarea4/src/svm/test_image_svm.py
@@ -1,20 +1,44 @@
 # Importa PIL para el manejo de la imagen
 import PIL
 from PIL import Image, ImageDraw, ImageOps
+import tkinter
+from tkinter import *
 import numpy as np
-# Importa joblib para cargar SVM
 import joblib
+import cv2
 
-# Ejemplo: svm_poly_5_3_0.05_0.001_-1.sav
-filename = input(
-    "Enter classifier to use: ")
 
-classifier = joblib.load("resource/" + filename)
-print('File used:', filename)
+def save():
+    filename = 'resource/number.png'
+    # segundo parámetro es 2 para bilineal o 3 para bicubico
+    image2 = image1.resize((28, 28), 2)
+    image2.save(filename)
+    predict()
 
-cont = True
-while cont:
 
+def reset():
+    global size
+    cv.delete("all")
+    draw.rectangle((0, 0, size, size), fill=(255))
+
+
+def activate_paint(e):
+    global lastx, lasty
+    cv.bind('<B1-Motion>', paint)
+    lastx, lasty = e.x, e.y
+
+
+def paint(e):
+    global lastx, lasty
+    thickness = 20
+    x, y = e.x, e.y
+    cv.create_oval((lastx, lasty, x, y), width=thickness)
+    #  --- PIL
+    draw.line((lastx, lasty, x, y), fill='black', width=thickness)
+    lastx, lasty = x, y
+
+
+def predict():
     im2 = PIL.Image.open("resource/number.png")
 
     convim2 = im2.convert('L')  # Lineal en escala de grises (normaliza datos)
@@ -29,6 +53,29 @@ while cont:
 
     print("Valor predicho:", predicted)
 
-    inputContinue = input("Scan again? (0-1): ")
-    if(inputContinue != '1'):
-        cont = False
+
+# + Main del archivo
+size = 200
+
+filename = input(
+    "Enter classifier to use: ")
+
+# Ejemplo: svm_poly_5_3_0.05_0.001_-1.sav
+classifier = joblib.load("resource/" + filename)
+print('File used:', filename)
+
+root = Tk()
+lastx, lasty = None, None
+cv = Canvas(root, width=size, height=size, bg='white')
+# --- PIL
+image1 = PIL.Image.new(mode='L', size=(size, size),
+                       color='white')  # L = escala de grises
+draw = ImageDraw.Draw(image1)
+cv.bind('<1>', activate_paint)
+cv.pack(expand=YES, fill=BOTH)
+
+btn_save = Button(text="save", command=save)
+btn_save.pack()
+btn_reset = Button(text="reset", command=reset)
+btn_reset.pack()
+root.mainloop()
